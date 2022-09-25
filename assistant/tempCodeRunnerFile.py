a78d38a01ee8d898627b5e640829127b4d49c3c8
siva sagar <81560062+gsivasagar@users.noreply.github.com>
2022-09-25 22:16:08 +0530

    Add files via upload

---
+++ assistant/tempCodeRunnerFile.py
@@ -0,0 +1,44 @@
+r.Recognizer()
+    engine = pyttsx3.init()
+    voice = engine.getProperty('voices')
+    engine.setProperty('voice', voice[2].id)
+
+    def talk(text):
+        engine.say(text)
+        engine.runAndWait()
+
+    def take_command():
+        try:
+            with sr.Microphone() as source:
+                print("listening...")
+                voice = listener.listen(source)
+                command = listener.recognize_google(voice)
+                command = command.lower()
+                if 'alexa' in command:
+                    command = command.replace('alexa', '')
+        except:
+            pass
+        return command
+
+    def run_alexa():
+        command = take_command()
+        print('command')
+        if 'play' in command:
+            song = command.replace('play', '')
+            talk('playing'+song)
+            pywhatkit.playonyt(song)
+        elif 'time' in command:
+            time = datetime.datetime.now().strftime('%H:%M')
+            talk('current time is' + time)
+        elif 'who' in command:
+            person = command.replace('who', '')
+            info = wikipedia.summary(person, 10)
+            talk(info)
+        elif 'what' in command:
+            thing = command.replace('what', '')
+            result = pywhatkit(thing, 10)
+            talk(result)
+        elif 'joke' in command:
+            talk(pyjokes.get_joke())
+
+    run_alexa()
